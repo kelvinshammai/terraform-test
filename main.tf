fdc082e6892d0b054e554b17fae8b77163a1d6cd
kelvinshammai <kelvinshammai@gmail.com>
2022-06-03 18:52:28 -0300

    test: teste para alterar o backend do terraform para o S3

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "shammai-tf-backend"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "null_resource" "cluster" {
   provisioner "local-exec" {
     command = "echo mentoria-iac"
